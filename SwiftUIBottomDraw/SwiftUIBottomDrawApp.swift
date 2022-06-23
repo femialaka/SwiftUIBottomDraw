18177530fee60a22ec112cca7211a7a0e580352f
femialaka <femialakadev@gmail.com>
2022-06-23 23:04:56 +0100

    Initial Commit

---
+++ SwiftUIBottomDraw/SwiftUIBottomDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBottomDrawApp.swift
+//  SwiftUIBottomDraw
+//
+//  Created by femi alaka on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBottomDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
