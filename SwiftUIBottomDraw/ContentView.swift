d176924aa453936dae890635114c5b38d14cbd11
femialaka <femialakadev@gmail.com>
2022-06-24 01:47:05 +0100

    Added Main Content area
    Added Bottom Sheet
    
    Added a drag gesture to the Bottom Modal sheet

--- SwiftUIBottomDraw/ContentView.swift
+++ SwiftUIBottomDraw/ContentView.swift
@@ -8,9 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    static let startWidth = UIScreen.main.bounds.size.width
+    static let startHeight = UIScreen.main.bounds.size.height / 12
+    @State private var dragWidth : CGFloat = startWidth
+    @State private var dragHeight : CGFloat = startHeight
+    
+    // keeps the panes a reasonable size based on device resolution
+    var minWidth : CGFloat = UIScreen.main.bounds.size.width / 6
+    let minHeight : CGFloat = UIScreen.main.bounds.size.height / 12
+    let maxHeight : CGFloat = UIScreen.main.bounds.size.height / 6
+    let screenWidth : CGFloat = UIScreen.main.bounds.size.width
+    
+    // purple and orange grips are this thick
+    let thickness : CGFloat = 9
+    
+    
+    // use computed properties to keep the body tidy
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        VStack(spacing: 0) {
+            mainContent
+            dragHandle
+            
+            VStack(spacing: 0) {
+                ZStack {
+                    Color.blue.edgesIgnoringSafeArea(.all)
+                    Text("Bottom Modal").font(.title2).bold()
+                }
+            }
+            .frame(height: dragHeight, alignment: .center)
+        }
+        
+        .frame(width: dragWidth)
+    }
+    
+    var mainContent : some View {
+        ZStack(alignment: .center) {
+            Color.green.edgesIgnoringSafeArea(.all)
+            Text("Main Content").font(.largeTitle).bold()
+        }
+    }
+    
+    
+    var dragHandle : some View {
+                Image(systemName: "line.3.horizontal")
+                    .padding(.top, 6)
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 28.0))
+                    .frame(width: self.screenWidth)
+                    .background(Color.blue)
+                
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                let screenHeight = UIScreen.main.bounds.size.height
+                                let delta = gesture.translation.height
+                                dragHeight = max(dragHeight - delta, minHeight)
+                                dragHeight = min(screenHeight - thickness - maxHeight, dragHeight)
+                            }
+                    )
+            
     }
 }
 
@@ -19,3 +77,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
